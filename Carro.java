ef1a684a79ccd11b6c1918eed8989e58a13f29bd
luaanHD <luantoniolo94@gmail.com>
2023-10-24 13:42:52 +0000

    Método Construtor

---
+++ Carro.java
@@ -0,0 +1,11 @@
+public class Carro{
+    private String cor;
+    private String modelo;
+    private String aro;
+    private String 
+    public Carro(){
+        
+    }
+    // 4 atributos para o Carro
+
+}
