642730088397cd8aa82989b251ab4758b06e335f
luaanHD <luantoniolo94@gmail.com>
2023-10-24 15:08:36 +0000

    Tarefa do Isaque dia 24/10/2023

--- Carro.java
+++ Carro.java
@@ -1,11 +1,57 @@
 public class Carro{
     private String cor;
     private String modelo;
-    private String aro;
-    private String 
+    private int aro;
+    private double valorCarro;
+
+    public int getAro() {
+        return aro;
+    }
+    public void setAro(int aro) {
+        this.aro = aro;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getValorCarro() {
+        return valorCarro;
+    }
+    public void setValorCarro(double valorCarro) {
+        this.valorCarro = valorCarro;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
     public Carro(){
         
     }
-    // 4 atributos para o Carro
+    public Carro(int aro){
+        this.aro=aro;
+    }
+    public Carro(int aro, String modelo){
+        this.aro=aro;
+        this.modelo=modelo;
+    }
+    public Carro(int aro, String modelo, double valorCarro){
+        this.aro=aro;
+        this.modelo=modelo;
+        this.valorCarro=valorCarro;
+    }
+    public Carro(int aro, String modelo, double valorCarro, String cor){
+        this.aro=aro;
+        this.modelo=modelo;
+        this.valorCarro=valorCarro;
+        this.cor=cor;
+    }
 
 }
--- Principal.java
+++ Principal.java
@@ -1,5 +1,17 @@
 public class Principal {
     public static void main(String[] args) {
         
+
+        Carro car = new Carro();
+        Carro carAro = new Carro(14);
+        Carro carTresParametros= new Carro(14, "HB20 2021 sense", 66600);
+        Carro carQuatroParamentros = new Carro(17, "Jetta GLi 2023", 232390, "cinza");
+
+        System.out.println(carAro.getAro());
+        System.out.println(carTresParametros.getAro() + " " + carTresParametros.getModelo() 
+        + " " + carTresParametros.getValorCarro());
+        System.out.println(carQuatroParamentros.getAro() 
+        + " " + carQuatroParamentros.getModelo() + " " + carQuatroParamentros.getValorCarro() 
+        + " " + carQuatroParamentros.getCor()); 
     }
 }
